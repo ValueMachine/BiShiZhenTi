6dfae23cf4d1a1a9818b199fce210ea09dcd1453
Ceisten <1220340922@qq.com>
2023-04-25 11:58:22 +0800

    新增华为一道题电动车充电动态规划

---
+++ src/华为/电动车充电.java
@@ -0,0 +1,10 @@
+package 华为;
+
+import java.util.Scanner;
+
+public class 电动车充电 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
+}
